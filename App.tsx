3fe0f2c757e3e38dff395d65ccee2420e0f6800b
Omar <omar.garcia@morsa.com.mx>
2026-02-17 10:27:01 -0600

    Factor de CRECIMIENTO FLEX 1

--- App.tsx
+++ App.tsx
@@ -21,26 +21,32 @@ const styles = StyleSheet.create({
     // Direccionamiento con FLEX, por default en MOBILE es COLUMN
     // flexDirection: 'row',
     // flexDirection: 'column',
+
+    // Poner FLEX: 1 en todos los hijos, hace que crezcan en la misma proporción, si el padre no tiene el FLEX: 1, no van a crecer en su totalidad.
   },
 
   caja1: {
     padding: 20,
     backgroundColor: 'navy',
+    flex: 1,
   },
 
   caja2: {
     padding: 20,
     backgroundColor: 'yellow',
+    flex: 1,
   },
 
   caja3: {
     padding: 20,
     backgroundColor: 'green',
+    flex: 1,
   },
 
   caja4: {
     padding: 20,
     backgroundColor: 'teal',
+    flex: 2,
   },
 });
 
